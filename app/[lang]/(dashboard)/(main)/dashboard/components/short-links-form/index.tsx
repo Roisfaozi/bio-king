0a83ca6904459cb58253460aec0f86607878bd5c
Roisfaozi <roisfaozi55@gmail.com>
2025-01-21 08:41:06 +0700

    Feat: create link form component

---
+++ app/[lang]/(dashboard)/(main)/dashboard/components/short-links-form/index.tsx
@@ -0,0 +1,27 @@
+import { Card, CardHeader } from '@/components/ui/card';
+import CardSnippet from '@/components/ui/card-snippet';
+import InputFormLink from './link-forms';
+
+const ShortLinksForm = () => {
+  return (
+    <div className='space-y-5'>
+      <Card>
+        <CardHeader className='mb-0 border-none'>
+          <div className='flex flex-wrap items-center gap-2'>
+            <div className='flex-1'>
+              <div className='whitespace-pre-wrap text-sm text-default-900'>
+                We are currently manually approving links. As soon as the link
+                is approved, you will be able to start using it.
+              </div>
+            </div>
+          </div>
+        </CardHeader>
+      </Card>
+      <CardSnippet>
+        <InputFormLink />
+      </CardSnippet>
+    </div>
+  );
+};
+
+export default ShortLinksForm;
