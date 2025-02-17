4281dc19ca690cf1d0c9733e06cb5ae5c9d30371
Roisfaozi <roisfaozi55@gmail.com>
2025-02-18 03:18:57 +0700

    Feat: add profile api route

---
+++ app/api/user/profile/route.ts
@@ -0,0 +1,130 @@
+import { getAuthSession } from '@/lib/auth';
+import db from '@/lib/db';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET(request: NextRequest, response: NextResponse) {
+  try {
+    const session = await getAuthSession();
+    const id = session?.user?.id;
+
+    if (!session) {
+      return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
+    }
+
+    const user = await db.user.findUnique({
+      where: { id: id },
+      include: { profile: true },
+    });
+
+    if (!user) {
+      return NextResponse.json({ message: 'User not found' }, { status: 404 });
+    }
+
+    if (user.role !== 'admin' && user.id !== session.user.id) {
+      return NextResponse.json(
+        { message: 'You do not have permission to access this resource' },
+        { status: 403 },
+      );
+    }
+
+    return NextResponse.json(user);
+  } catch (error) {
+    console.log('An error occurred:', error);
+    return NextResponse.json(
+      { message: 'Something went wrong' },
+      { status: 500 },
+    );
+  }
+}
+
+export async function PUT(request: NextRequest, response: NextResponse) {
+  try {
+    const session = await getAuthSession();
+    const id = session?.user?.id;
+
+    const data = await request.json();
+
+    if (!session) {
+      return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
+    }
+
+    const user = await db.user.findUnique({
+      where: { id: id },
+      include: { profile: true },
+    });
+
+    if (!user) {
+      return NextResponse.json({ message: 'User not found' }, { status: 404 });
+    }
+
+    if (user.role !== 'admin' && user.id !== session.user.id) {
+      return NextResponse.json(
+        { message: 'You do not have permission to access this resource' },
+        { status: 403 },
+      );
+    }
+
+    const updatedUser = await db.user.update({
+      where: { id: id },
+      data: {
+        profile: {
+          update: {
+            ...data,
+          },
+        },
+      },
+    });
+
+    return NextResponse.json(updatedUser);
+  } catch (error) {
+    console.log('An error occurred:', error);
+    return NextResponse.json(
+      { message: 'Something went wrong' },
+      { status: 500 },
+    );
+  }
+}
+
+export async function DELETE(request: NextRequest, response: NextResponse) {
+  try {
+    const session = await getAuthSession();
+    const id = session?.user?.id;
+
+    if (!session) {
+      return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
+    }
+
+    const user = await db.user.findUnique({
+      where: { id: id },
+      include: { profile: true },
+    });
+
+    if (!user) {
+      return NextResponse.json({ message: 'User not found' }, { status: 404 });
+    }
+
+    if (user.role !== 'admin' && user.id !== session.user.id) {
+      return NextResponse.json(
+        { message: 'You do not have permission to access this resource' },
+        { status: 403 },
+      );
+    }
+
+    await db.user.update({
+      where: { id: id },
+      data: {
+        profile: {
+          delete: true,
+        },
+      },
+    });
+
+    return NextResponse.json({ message: 'Profile deleted successfully' });
+  } catch (error) {
+    console.log('An error occurred:', error);
+    return NextResponse.json(
+      { message: 'Something went wrong' },
+      { status: 500 },
+    );
+  }
+}
