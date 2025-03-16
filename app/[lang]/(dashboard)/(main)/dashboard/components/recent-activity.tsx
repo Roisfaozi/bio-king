22132f3d8df56ffc67617c8708813b9111193fd5
Roisfaozi <roisfaozi55@gmail.com>
2025-03-16 12:03:31 +0700

    refactor: refactor recent activity component

--- app/[lang]/(dashboard)/(main)/dashboard/components/recent-activity.tsx
+++ app/[lang]/(dashboard)/(main)/dashboard/components/recent-activity.tsx
@@ -2,179 +2,24 @@
 import DashboardDropdown from '@/components/dashboard-dropdown';
 import { Web } from '@/components/svg';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import {
+  ClickActivity,
+  RecentActivities,
+  RecentActivityType,
+  RecentCliksResponse,
+} from '@/models/click-resonse';
+import { format } from 'date-fns';
+import { epochToDate } from '@/lib/utils';
+import { credentialsConfig } from '@/config/credentials.config';
+import { Chrome, Facebook, Monitor } from 'lucide-react';
 
-type Activity = {
-  id: number;
-  type: string;
-  title: string;
-  url: string;
-  visited: string;
-  statistik: {
-    city: string;
-    country: string;
-    os: string;
-    browser: string;
-    refferer: string;
-    language: string;
-  };
-};
 interface RecentActivityProps {
-  activities: Activity[];
+  recentActivity: RecentActivities;
 }
 
-const activities = [
-  {
-    id: 1,
-    type: 'Bio Page',
-    title: 'My Own Bio',
-    url: 'https://lele.co/abc',
-    visited: '12 - 12 - 2024',
-    statistik: {
-      city: 'bandung',
-      country: 'indonesia',
-      os: 'windows 10',
-      browser: 'Chrome',
-      refferer: 'facebook',
-      language: 'En',
-    },
-  },
-  {
-    id: 2,
-    type: 'Bio Page',
-    title: 'My Own Bio',
-    url: 'https://lele.co/abc',
-    visited: '12 - 12 - 2024',
-    statistik: {
-      city: 'bandung',
-      country: 'indonesia',
-      os: 'windows 10',
-      browser: 'Chrome',
-      refferer: 'facebook',
-      language: 'En',
-    },
-  },
-  {
-    id: 3,
-    type: 'Bio Page',
-    title: 'My Own Bio',
-    url: 'https://lele.co/abc',
-    visited: '12 - 12 - 2024',
-    statistik: {
-      city: 'bandung',
-      country: 'indonesia',
-      os: 'windows 10',
-      browser: 'Chrome',
-      refferer: 'facebook',
-      language: 'En',
-    },
-  },
-  {
-    id: 4,
-    type: 'Bio Page',
-    title: 'My Own Bio',
-    url: 'https://lele.co/abc',
-    visited: '12 - 12 - 2024',
-    statistik: {
-      city: 'bandung',
-      country: 'indonesia',
-      os: 'windows 10',
-      browser: 'Chrome',
-      refferer: 'facebook',
-      language: 'En',
-    },
-  },
-  {
-    id: 5,
-    type: 'Bio Page',
-    title: 'My Own Bio',
-    url: 'https://lele.co/abc',
-    visited: '12 - 12 - 2024',
-    statistik: {
-      city: 'bandung',
-      country: 'indonesia',
-      os: 'windows 10',
-      browser: 'Chrome',
-      refferer: 'facebook',
-      language: 'En',
-    },
-  },
-  {
-    id: 6,
-    type: 'Bio Page',
-    title: 'My Own Bio',
-    url: 'https://lele.co/abc',
-    visited: '12 - 12 - 2024',
-    statistik: {
-      city: 'bandung',
-      country: 'indonesia',
-      os: 'windows 10',
-      browser: 'Chrome',
-      refferer: 'facebook',
-      language: 'En',
-    },
-  },
-  {
-    id: 7,
-    type: 'Bio Page',
-    title: 'My Own Bio',
-    url: 'https://lele.co/abc',
-    visited: '12 - 12 - 2024',
-    statistik: {
-      city: 'bandung',
-      country: 'indonesia',
-      os: 'windows 10',
-      browser: 'Chrome',
-      refferer: 'facebook',
-      language: 'En',
-    },
-  },
-  {
-    id: 8,
-    type: 'Bio Page',
-    title: 'My Own Bio',
-    url: 'https://lele.co/abc',
-    visited: '12 - 12 - 2024',
-    statistik: {
-      city: 'bandung',
-      country: 'indonesia',
-      os: 'windows 10',
-      browser: 'Chrome',
-      refferer: 'facebook',
-      language: 'En',
-    },
-  },
-  {
-    id: 9,
-    type: 'Bio Page',
-    title: 'My Own Bio',
-    url: 'https://lele.co/abc',
-    visited: '12 - 12 - 2024',
-    statistik: {
-      city: 'bandung',
-      country: 'indonesia',
-      os: 'windows 10',
-      browser: 'Chrome',
-      refferer: 'facebook',
-      language: 'En',
-    },
-  },
-  {
-    id: 10,
-    type: 'Bio Page',
-    title: 'My Own Bio',
-    url: 'https://lele.co/abc',
-    visited: '12 - 12 - 2024',
-    statistik: {
-      city: 'bandung',
-      country: 'indonesia',
-      os: 'windows 10',
-      browser: 'Chrome',
-      refferer: 'facebook',
-      language: 'En',
-    },
-  },
-];
-const RecentActivity = () => {
+const RecentActivity = ({ recentActivity }: RecentActivityProps) => {
+  console.log(recentActivity);
+
   return (
     <Card>
       <CardHeader className='flex-row items-center justify-between border-none pb-0'>
@@ -183,7 +28,7 @@ const RecentActivity = () => {
       </CardHeader>
       <CardContent className='px-0'>
         <div>
-          {activities.map((item, index) => (
+          {recentActivity.map((item, index) => (
             <ActivityList activity={item} key={`social-item-${index}`} />
           ))}
         </div>
@@ -195,7 +40,7 @@ const RecentActivity = () => {
 export default RecentActivity;
 
 interface ActivityListProps {
-  activity: Activity;
+  activity: RecentActivityType;
 }
 
 function ActivityList({ activity }: ActivityListProps) {
@@ -215,16 +60,24 @@ function ActivityList({ activity }: ActivityListProps) {
                   {activity.title}
                 </p>
               </div>
-              <p className='text-md font-normal text-default-600'>
-                {activity.url}
-              </p>
+              <a
+                href={activity.url}
+                target='_blank'
+                rel='noopener noreferrer'
+                className='text-md font-normal text-default-600 hover:text-primary-400'
+              >
+                {credentialsConfig.siteUrl + activity.url}
+              </a>
             </div>
           </div>
         </div>
         <div className='flex items-start'>
           <div className='p-1'>
             <p className='text-sm font-normal text-secondary-foreground'>
-              {activity.visited}
+              {format(
+                epochToDate(Number(activity.visited_at)),
+                'MMM d, yyyy HH:mm',
+              )}
             </p>
           </div>
         </div>
@@ -232,34 +85,34 @@ function ActivityList({ activity }: ActivityListProps) {
       <div className='flex w-full flex-wrap gap-2'>
         <div className='flex items-center gap-2'>
           <div className='h-5 w-5'>
-            <Web />
+            <Web className='h-4 w-4 text-gray-400' />
           </div>
           <p className='text-sm font-normal text-secondary-foreground'>
-            {`${activity.statistik.city}, ${activity.statistik.country}`}
+            {`${activity.city}, ${activity.country}`}
           </p>
         </div>
         <div className='flex items-center gap-2'>
           <div className='h-5 w-5'>
-            <Web />
+            <Monitor className='h-4 w-4 text-gray-400' />
           </div>
           <p className='text-sm font-normal text-secondary-foreground'>
-            {activity.statistik.os}
+            {activity.os}
           </p>
         </div>
         <div className='flex items-center gap-2'>
           <div className='h-5 w-5'>
-            <Web />
+            <Chrome className='h-4 w-4 text-gray-400' />
           </div>
           <p className='text-sm font-normal text-secondary-foreground'>
-            {activity.statistik.browser}
+            {activity.browser}
           </p>
         </div>
         <div className='flex items-center gap-2'>
           <div className='h-5 w-5'>
-            <Web />
+            <Facebook className='h-4 w-4 text-gray-400' />
           </div>
           <p className='text-sm font-normal text-secondary-foreground'>
-            {activity.statistik.refferer}
+            {activity.referrer}
           </p>
         </div>
       </div>
